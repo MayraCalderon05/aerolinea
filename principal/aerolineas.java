781a4135687408736162518a8a8ee28cdbfed4db
MayraCalderon05 <may2005_cc@hotmail.com>
2025-11-20 00:41:58 -0300

    habia un tema con la validez, validacion correcta

--- principal/aerolineas.java
+++ principal/aerolineas.java
@@ -18,23 +18,30 @@ public class aerolineas {
 
     // metodo para agregar un nuevo avion
     public static void agregarAvion(Avion[] aviones) {
-        
-        
+
+        String identificacion;
+        String modelo;
+        int cantVuelos;
+        int cantAsientos;
+        double kmRecorridos;
+
+        System.out.println("Ingrese la identificacion del avion:");
 
     }
 
     public static boolean validarIdAvion(String id) {
-        //si no esta bien estructurado no entra en ningun condicional y retorna falso
+        // si no esta bien estructurado no entra en ningun condicional y retorna falso
         boolean valido = false;
 
-        // ?verifico que tenga el guion en el medio y que no se pase de caracteres y ahi analizo el resto
+        // ?verifico que tenga el guion en el medio y que no se pase de caracteres y ahi
+        // analizo el resto
         if (existeGuion(id)) {
             // la posicion 2 es del guion, no la toma en el substring
             String iniciales = id.substring(0, 2);
             // me refiero al resto de la cadena despues de las iniciales
             String resto;
 
-            //analizo las primeras dos iniciales
+            // analizo las primeras dos iniciales
             switch (iniciales) {
                 case "LV":
                     char postGuion = id.charAt(3);
@@ -73,10 +80,10 @@ public class aerolineas {
                     }
                     break;
                 default:
-                valido = false;
-                break;
+                    valido = false;
+                    break;
             }
-        } 
+        }
         return valido;
     }
 
@@ -84,28 +91,36 @@ public class aerolineas {
         int[] numeros = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         boolean valido = true;
         int i = 0;
+        if (resto.length() <= 3) {
+            while (valido && i < resto.length()) {
+                char caracter = resto.charAt(i);
 
-        while (valido && resto.length() <= 3 && i < resto.length()) {
-            char caracter = resto.charAt(i);
+                // verifico que sea un digito numerico por CODIGO ASCII
+                if (caracter >= '0' && caracter <= '9') {
+                    int digito = Integer.parseInt(String.valueOf(caracter));
+                    int j = 0;
+                    boolean noEncontrado = true;
+                    // solo verifica que sean numeros, para el caso que necesite numeros en el resto
+                    // de los digitos
+                    do {
+                        if (digito != numeros[j]) {
+                            noEncontrado = false;
+                        }
+                        j++;
+                    } while (j < numeros.length && noEncontrado);
 
-            //verifico que sea un digito numerico por CODIGO ASCII
-            if (caracter >= '0' && caracter <= '9') {
-                int digito = Integer.parseInt(String.valueOf(caracter));
-                int j = 0;
-                boolean noEncontrado = true;
-                // solo verifica que sean numeros, para el caso que necesite numeros en el resto
-                // de los digitos
-                do {
-                    if (digito != numeros[j]) {
-                        noEncontrado = false;
+                    if (!noEncontrado) {
+                        valido = false;
+                    } else {
+                        i++;
+
                     }
-                    j++;
-                } while (j < numeros.length && noEncontrado);
-                
-                i++;
-            } else {
-                valido = false;
+                } else {
+                    valido = false;
+                }
             }
+        } else {
+            valido = false;
         }
 
         return valido;
